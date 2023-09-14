79f720469e89d87d7d8a6eb59ceb19bcde8496c2
Yingxue Huang <janehuang@Yingxues-MacBook-Pro.local>
2023-09-14 15:37:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require("express")
+const session = require('express-session')
 const app = express()
 
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.set("view engine", "ejs")
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "tgtg"
+}))
+
+const ownerAccountSettingsRouter = require("./routes/owners/owners")
+const ownerBagsRouter = require("./routes/owners/ownerBags")
+const ownerOrdersRouter = require("./routes/owners/ownerOrders")
+
+const buyerAccountSettingsRouter = require("./routes/buyers/buyers")
+const buyerBagsRouter = require("./routes/buyers/buyerBags")
+const buyerOrdersRouter = require("./routes/buyers/buyerOrders")
+const paymentRouter = require("./routes/buyers/payment")
+
+app.use("/owners", ownerAccountSettingsRouter)
+app.use("/ownerBags", ownerBagsRouter)
+app.use("/ownerOrders", ownerOrdersRouter)
+
+app.use("/buyers", buyerAccountSettingsRouter)
+app.use("/buyerBags", buyerBagsRouter)
+app.use("/buyerOrders", buyerOrdersRouter)
+app.use("/payment", paymentRouter)
 
 app.listen(3000)
